d352e97b537921313017cb6a0b42c33b266c418f
root <root@puppet-master>
2018-08-25 01:02:32 +0800

    Use "if ! defined" to prevent dublicated resource.x

--- manifests/openjdk_java.pp
+++ manifests/openjdk_java.pp
@@ -3,30 +3,36 @@ class corp104_karaf::openjdk_java inherits corp104_karaf {
   $package_jdk = 'openjdk-8-jdk'
   $java_home    = '/usr/lib/jvm/java-8-openjdk-amd64'
 
-  $add_apt_package = [ 'python-software-properties', 'software-properties-common' ]
-  Package <| title == $add_apt_package |> {
-    ensure => present,
-    notify => Exec['install-ppa'],
+  $add_apt_packages = [ 'python-software-properties', 'software-properties-common' ]
+  $add_apt_packages.each | String $add_apt_package| {
+    if ! defined(Package[$add_apt_package]){
+      package { $add_apt_package:
+        ensure => present,
+        notify => Exec['install-ppa'],
+      }
+    }
   }
 
-  if $corp104_karaf::http_proxy {
-    Exec <| title == 'install-ppa' |> {
-      path        => '/bin:/usr/sbin:/usr/bin:/sbin',
-      environment => [
-        "http_proxy=${corp104_karaf::http_proxy}",
-      ],
-      command     => "add-apt-repository -y ${corp104_karaf::ppa_openjdk} && apt-get update",
-      user        => 'root',
-      unless      => "/usr/bin/dpkg -l | grep ${package_jre}",
-      before      => Package[$package_jre],
-    }
-  } else {
-    Exec <| title == 'install-ppa' |> {
-      path    => '/bin:/usr/sbin:/usr/bin:/sbin',
-      command => "add-apt-repository -y ${corp104_karaf::ppa_openjdk} && apt-get update",
-      user    => 'root',
-      unless  => "/usr/bin/dpkg -l | grep ${package_jre}",
-      before  => Package[$package_jre],
+  if ! defined(Exec['install-ppa']) {
+    if $corp104_karaf::http_proxy {
+      exec { 'install-ppa':
+        path        => '/bin:/usr/sbin:/usr/bin:/sbin',
+        environment => [
+          "http_proxy=${corp104_karaf::http_proxy}",
+        ],
+        command     => "add-apt-repository -y ${corp104_karaf::ppa_openjdk} && apt-get update",
+        user        => 'root',
+        unless      => "/usr/bin/dpkg -l | grep ${package_jre}",
+        before      => Package[$package_jre],
+      }
+    } else {
+      exec { 'install-ppa':
+        path    => '/bin:/usr/sbin:/usr/bin:/sbin',
+        command => "add-apt-repository -y ${corp104_karaf::ppa_openjdk} && apt-get update",
+        user    => 'root',
+        unless  => "/usr/bin/dpkg -l | grep ${package_jre}",
+        before  => Package[$package_jre],
+      }
     }
   }
 
