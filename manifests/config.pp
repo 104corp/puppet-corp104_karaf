1f2142529be0c8b54c4f37ea2d3c81005d0094e3
genchi.lu[盧俊錡] <genchi.lu@genchilu.local>
2018-12-25 11:58:04 +0800

    Remove unnecessary code.

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,11 +2,12 @@ class corp104_karaf::config inherits corp104_karaf {
 
   exec { 'upgrad cxf':
     provider    => 'shell',
-    environment => ["JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64"],
     command     => "${corp104_karaf::install_path}/bin/karaf feature:repo-remove cxf && ${corp104_karaf::install_path}/bin/karaf feature:repo-add cxf 3.2.4",
     path        => '/bin:/usr/bin:/usr/local/bin:/usr/sbin',
+    subscribe   => File['karaf'],
     refreshonly => true,
-    subscribe   => File['karaf']
+    tries       => 3,
+    try_sleep   => 1,
   }
 
   $required_features = ['webconsole', 'cxf', 'cxf-jaxrs', 'http-whiteboard', 'http', 'cxf-jaxws', 'aries-blueprint']
